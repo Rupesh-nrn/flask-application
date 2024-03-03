bc5032b21011ba6b590037b08e6c0e1cb9bc9257
rupesh <rupesh25ai047@satiengg.in>
2024-03-03 14:14:42 +0530

    add new route

--- flask.py
+++ flask.py
@@ -1,10 +1,14 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def greet():
     return 'Hello, World!'
+
+@app.route('/greet')
+def greet_me():
+    return 'Hi therr'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
